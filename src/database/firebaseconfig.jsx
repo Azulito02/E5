e7a687d013a2d815456c37a81aab1407145cc659
Milton02 <miltonestudio023@gmail.com>
2025-04-10 09:06:53 -0600

    sin conexion

--- src/database/firebaseconfig.jsx
+++ src/database/firebaseconfig.jsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; // Asegúrate de importar esto
 import {getAuth} from "firebase/auth"
+import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAr7pTesPaRylZogIQ-uskkzZO_SJvVSZE",
@@ -12,9 +13,24 @@ const firebaseConfig = {
   measurementId: "G-0FRPXZEB3N"
 };
 
+
+
 const appfirebase = initializeApp(firebaseConfig);
 
-const db = getFirestore(appfirebase);
+let db;
+try {
+  db = initializeFirestore(appfirebase, {
+    localCache: persistentLocalCache({
+      cacheSizeBytes: 100 * 1024 * 1024, // 100 MB (opcional, para limitar tamaño)
+    }),
+  });
+  console.log("Firestore inicializado con persistencia offline.");
+} catch (error) {
+  console.error("Error al inicializar Firestore con persistencia:", error);
+  // Fallback: inicializar sin persistencia si falla
+  db = initializeFirestore(appfirebase, {});
+}
+
 
 const auth = getAuth(appfirebase);
 
